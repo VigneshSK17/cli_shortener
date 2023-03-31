c1e6fc04bba7f9057d330a29f74d4628fcd1c118
VigneshSK17 <vigsk17@gmail.com>
2023-03-30 22:50:50 -0400

    Added axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::routing;
+
+
+
+#[tokio::main]
+async fn main() {
+
+    let app = axum::Router::new()
+        .route("/", routing::get(test));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+}
+
+async fn test() -> &'static str {
+    "Welcome to cli-shortener!"
 }
