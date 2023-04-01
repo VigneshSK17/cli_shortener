466706afe547cc174f8940307746be15438ddc45
VigneshSK17 <vigsk17@gmail.com>
2023-03-31 22:02:28 -0400

    Added setup for sqlite db

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,57 @@
-use std::{path::PathBuf, env};
+use std::{fs::File, path::PathBuf, env};
+
+use sqlx::SqlitePool;
 
 
-pub fn get_db_path() {
+pub fn get_db_path() -> (PathBuf, String) {
 
     let mut path = dirs::data_local_dir()
         .unwrap_or(env::current_dir().expect("Could not find a directory to store links"));
 
     path.push("cli_shortener/links.db");
 
+    if !path.is_file() {
+        File::create(&path).expect("Could not create database to store links");
+    }
+
     tracing::info!("{:?}", path);
+
+    let path_str = path.to_string_lossy().into_owned();
+
+    (path, path_str)
+
+}
+
+// pub async fn create_db(path: &PathBuf) {
+//
+//     let path_str = path.to_string_lossy();
+//
+//     if !Sqlite::database_exists(&path_str).await.unwrap_or(false) {
+//
+//         Sqlite::create_database(&path_str).await
+//             .expect("Could not create database to store links");
+//
+//         create_schema(&path_str).await;
+//
+//     }
+//
+//     tracing::info!("Database already exists at {}", path_str);
+//
+// } 
+
+pub async fn create_schema(pool: &SqlitePool) {
+
+    sqlx::query(r#"
+        CREATE TABLE IF NOT EXISTS links (
+            link text,
+            hash text
+        );
+    "#)
+        .execute(pool)
+        .await
+        .expect("Could not create table inside database");
+
+    // tracing::info!("Initialized database at {}");
+
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Duration};
 
-use axum::{routing, response::{IntoResponse, Redirect}};
+use axum::{routing, response::{IntoResponse, Redirect}, extract::State};
+use sqlx::{SqlitePool, sqlite::SqlitePoolOptions};
 
 mod db;
 
@@ -9,8 +10,24 @@ async fn main() {
 
     tracing_subscriber::fmt::init();
 
+    let (db_url, db_url_str) = db::get_db_path();
+
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .idle_timeout(Duration::from_secs(3))
+        .connect(&db_url_str)
+        .await
+        .expect("Could not connect to database");
+
+    db::create_schema(&pool)
+        .await;
+
+    tracing::info!("Initialized database at {}", db_url_str);
+
+
     let app = axum::Router::new()
-        .route("/", routing::get(test));
+        .route("/", routing::get(test_db))
+        .with_state(pool);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     tracing::info!("listening on {}", addr);
@@ -23,9 +40,17 @@ async fn main() {
 }
 
 async fn test() -> impl IntoResponse {
-    db::get_db_path();
+    let path = db::get_db_path();
+
     "Welcome to cli-shortener!"
     
     // Redirect::permanent("https://www.google.com")
 
+}
+
+async fn test_db(
+    State(pool): State<SqlitePool>
+) -> impl IntoResponse {
+
+    "Hi"
 }
