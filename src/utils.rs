bfa07539af4e4150152fa7a7de7bd6f5d5635574
VigneshSK17 <vigsk17@gmail.com>
2024-07-21 15:44:12 -0400

    Updated the url checking regex and added unit testing to verify

--- src/utils.rs
+++ src/utils.rs
@@ -3,18 +3,40 @@ use serde::{Deserialize, Serialize};
 pub fn gen_hash() -> String {
     format!(
         "{}-{}",
-        random_word::gen_len(7).expect("Could not generate hash"),
-        random_word::gen_len(7).expect("Could not generate hash")
+        random_word::gen_len(5).expect("Could not generate hash"),
+        random_word::gen_len(5).expect("Could not generate hash")
     )
 }
 
 pub fn is_url(url: &str) -> bool {
     const REGEX: &str = r"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
-
     regex::Regex::new(REGEX).unwrap().is_match(url)
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct CreateLink {
     pub link: String,
+}
+
+#[test]
+fn test_is_url() {
+    let ok_urls = [
+        "http://foo.com/blah_blah",
+        "http://foo.com/blah_blah",
+        "http://foo.com/blah_blah/",
+        "http://foo.com/blah_blah_(wikipedia)",
+        "http://foo.com/blah_blah_(wikipedia)_(again)",
+        "http://www.example.com/wpstyle/?p=364",
+        "https://www.example.com/foo/?bar=baz&inga=42&quux",
+        "http://code.google.com/events/#&product=browser",
+    ];
+
+    let bad_urls = ["http://", "//a", "foo.com", "h://test"];
+
+    for url in ok_urls.iter() {
+        assert_eq!(is_url(url), true);
+    }
+    for url in bad_urls.iter() {
+        assert_eq!(is_url(url), false);
+    }
 }
